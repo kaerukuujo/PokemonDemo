41ecbd4edb3fb16f464b918e3caba9fe94f3895d
kaerukuujo <mrjolyne@gmail.com>
2023-01-01 15:31:36 +0000

    added battle animations + UI for move selection

--- index.js
+++ index.js
@@ -79,7 +79,8 @@ const player = new Sprite({
     },
     image: playerDownImage,
     frames: {
-        max: 4
+        max: 4,
+        hold: 10
     },
     sprites: {
         up: playerUpImage,
@@ -149,7 +150,7 @@ function animate() {
     foreground.draw();
 
     let moving = true;
-    player.moving = false;
+    player.animate = false;
 
     console.log(animationId);
     if(battle.initiated) return
@@ -204,7 +205,7 @@ function animate() {
     };   
     
     if(keys.w.pressed && lastKey === 'w'){
-        player.moving = true;
+        player.animate = true;
         player.image = player.sprites.up;
 
         for(let i = 0; i < boundaries.length; i++) {
@@ -229,7 +230,7 @@ function animate() {
                 movable.position.y += 3;
             });
         } else if(keys.a.pressed  && lastKey === 'a'){
-            player.moving = true;
+            player.animate = true;
             player.image = player.sprites.left;
             for(let i = 0; i < boundaries.length; i++) {
                 const boundary = boundaries[i];
@@ -252,7 +253,7 @@ function animate() {
                 movable.position.x += 3;
             });
         } else if(keys.s.pressed  && lastKey === 's'){
-            player.moving = true;
+            player.animate = true;
             player.image = player.sprites.down;
             for(let i = 0; i < boundaries.length; i++) {
                 const boundary = boundaries[i];
@@ -275,7 +276,7 @@ function animate() {
                 movable.position.y -= 3;
             });
         } else if(keys.d.pressed  && lastKey === 'd'){
-            player.moving = true;
+            player.animate = true;
             player.image = player.sprites.right;
             for(let i = 0; i < boundaries.length; i++) {
                 const boundary = boundaries[i];
@@ -311,11 +312,44 @@ const battleBackground = new Sprite({
     image: battleBackgroundImage
 })
 
+const draggleImage = new Image();
+draggleImage.src = './img/draggleSprite.png'
+const draggle = new Sprite({
+    position: {
+        x: 800,
+        y: 100
+    },
+    image: draggleImage,
+    frames: {
+        max: 4,
+        hold: 30
+    },
+    animate: true  
+})
+
+const embyImage = new Image();
+embyImage.src = './img/embySprite.png';
+const emby = new Sprite({
+    position: {
+        x: 280,
+        y: 325
+    },
+    image: embyImage,
+    frames: {
+        max: 4, 
+        hold: 30
+    },
+    animate: true
+})
+
 function animateBattle(){
     window.requestAnimationFrame(animateBattle);
     battleBackground.draw();
+    draggle.draw();
+    emby.draw();
 }
 
+// animate();
 animateBattle();
 
 let lastKey = '';
